5e2dc8b727f6221683d2f53d880ff4630cbc9d33
girunivlille <girardmathilde2000@gmail.com>
2025-04-03 16:02:22 +0200

    fix fastq bug

--- copy_readsfile.cpp
+++ copy_readsfile.cpp
@@ -28,27 +28,33 @@ vector<string> split_string(const string& str, char separator) {
     return result;
 }
 
-void copy_the_file(const string& reads_file,const string& readsfile_copy){
+void copy_the_file(const string& reads_file,const string& readsfile_copy, const string& format){
     // string readsfile_copy="";
     vector<string> repo_files = split_string(reads_file, '/');
     // readsfile_copy = "copie-"+repo_files[repo_files.size()-1];
     fstream reads(reads_file, ios::in);
     fstream copy_file(readsfile_copy, ios::out);
-    int compteur=0;
-    string ajout;
+    int cmpt=0;
+    string add_to_file;
     if(reads && copy_file){
         string line;
         while(!reads.eof()){
             getline(reads, line);
-            if(line.length()>0 and line[0]=='>'){
-                //la ligne est un id
-                ajout = ">"+to_string(compteur);
-                copy_file << ajout << endl;
-                compteur++;
+            if(line.length()>0){
+                // write header and sequence
+		        add_to_file = ">"+to_string(cmpt);
+		        copy_file << add_to_file << endl;
+		        getline(reads, line);
+		        copy_file << line << endl;
+                cmpt++;
+                if (format=="fastq"){
+                    getline(reads, line);
+                    getline(reads, line);
+                }
             }
-            else if(line.length()>0){
-                copy_file << line << endl;
-            }
+            //else if(line.length()>0){
+            //    copy_file << line << endl;
+            //}
         }
     }
     else{
@@ -65,16 +71,18 @@ int main(int argc, char *argv[])
 {
     string readsfile;
     string readsfile_copy;
-    if (argc!=3){
+    string format;
+    if (argc!=4){
         cerr << "Mauvais nombre d'arguments" << endl;
         return EXIT_FAILURE;
     }
     else{
         readsfile = argv[1];
         readsfile_copy = argv[2];
+	format = argv[3];
     }
 
-    copy_the_file(readsfile,readsfile_copy);
+    copy_the_file(readsfile,readsfile_copy,format);
     return 0;
 }
 
